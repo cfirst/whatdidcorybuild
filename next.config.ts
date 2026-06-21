fd63a8de5e7ba956310c0ac277dca25b69cb8b3e
Cory Firstenberg <coryfirstenberg@Corys-MacBook-Pro.local>
2026-06-20 18:41:51 -0700

    fix: add Spotify image domains to next.config

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,22 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "i.scdn.co",
       },
+      {
+        protocol: "https",
+        hostname: "mosaic.scdn.co",
+      },
+      {
+        protocol: "https",
+        hostname: "image-cdn-ak.spotifycdn.com",
+      },
+      {
+        protocol: "https",
+        hostname: "image-cdn-fa.spotifycdn.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lineup-images.scdn.co",
+      },
     ],
   },
 };
