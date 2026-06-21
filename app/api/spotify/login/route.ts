e7fbf2a622ea70fdc2338ee4b215b15ac23a1ea0
Cory Firstenberg <coryfirstenberg@Corys-MacBook-Pro.local>
2026-06-20 20:58:16 -0700

    fix: add user-library-read scope

--- app/api/spotify/login/route.ts
+++ app/api/spotify/login/route.ts
@@ -4,8 +4,7 @@ export async function GET() {
   const clientId = process.env.SPOTIFY_CLIENT_ID!
   const redirectUri = process.env.SPOTIFY_REDIRECT_URI!
 
-  const scope = 'user-read-currently-playing user-read-recently-played playlist-read-private playlist-read-collaborative'
-
+const scope = 'user-read-currently-playing user-read-recently-played playlist-read-private playlist-read-collaborative user-library-read'
   const params = new URLSearchParams({
     client_id: clientId,
     response_type: 'code',
